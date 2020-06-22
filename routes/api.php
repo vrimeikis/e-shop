3da54d3b5bbf2d0a2470b7f2ffb3a89d2d9a3d9c
vytautas <vytautas.rimeikis@gmail.com>
2020-06-22 21:15:22 +0300

    api register login logout

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('API\Auth')->prefix('auth')->group(function () {
+    Route::post('register', 'RegisterController@register');
+    Route::post('login', 'LoginController@login');
+    Route::post('logout', 'LoginController@logout');
+});
