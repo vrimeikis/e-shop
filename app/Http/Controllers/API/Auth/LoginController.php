6c214a5471b08e2cae5d44825b64fbf192873086
vytautas <vytautas.rimeikis@gmail.com>
2020-07-13 22:01:56 +0300

    Fix api login

--- app/Http/Controllers/API/Auth/LoginController.php
+++ app/Http/Controllers/API/Auth/LoginController.php
@@ -11,9 +11,14 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Passport\Token;
 use Lcobucci\JWT\Parser;
 
+/**
+ * Class LoginController
+ * @package App\Http\Controllers\API\Auth
+ */
 class LoginController extends Controller
 {
     use AuthenticatesUsers;
@@ -29,7 +34,7 @@ class LoginController extends Controller
     /**
      * Send the response after the user was authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response|RedirectResponse
      */
     protected function sendLoginResponse(Request $request)
@@ -48,8 +53,8 @@ class LoginController extends Controller
     /**
      * The user has been authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  Authenticatable|User  $user
+     * @param \Illuminate\Http\Request $request
+     * @param Authenticatable|User $user
      * @return mixed
      */
     protected function authenticated(Request $request, $user)
@@ -69,9 +74,31 @@ class LoginController extends Controller
      */
     protected function guard()
     {
-        return Auth::guard();
+        return Auth::guard('api');
     }
 
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        $user = User::query()->where('email', $request->email)->first();
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                $this->guard()->setUser($user);
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
     public function logout(Request $request)
     {
         $value = $request->bearerToken();
