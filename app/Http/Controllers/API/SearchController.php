5e3c16a95b726015fc28d7393bfd11fae25ce699
vytautas <vytautas.rimeikis@gmail.com>
2020-07-27 22:00:16 +0300

    search

--- app/Http/Controllers/API/SearchController.php
+++ app/Http/Controllers/API/SearchController.php
@@ -2,17 +2,47 @@
 
 namespace App\Http\Controllers\API;
 
+use App\DTO\Base\CollectionDTO;
+use App\DTO\Base\PaginateLengthAwareDTO;
+use App\DTO\ProductDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\SearchRequest;
 use App\Product;
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\JsonResponse;
 
+/**
+ * Class SearchController
+ * @package App\Http\Controllers\API
+ */
 class SearchController extends Controller
 {
-    public function search(SearchRequest $request)
+    /**
+     * @param SearchRequest $request
+     * @return JsonResponse
+     */
+    public function search(SearchRequest $request): JsonResponse
     {
-        return Product::query()
-            ->where('title', 'LIKE', $request->getQuery())
+        $searchPhrase = $request->getQuery();
+        $products = Product::query()
+            ->with(['categories', 'featureValues', 'featureValues.feature'])
+            ->where('title', 'LIKE', $searchPhrase)
+            ->orWhereHas('categories', function (Builder $q) use ($searchPhrase) {
+                $q->where('title', 'LIKE', $searchPhrase);
+            })
+            ->orderByDesc('created_at')
             ->paginate();
+
+
+        $paginateDTO = new PaginateLengthAwareDTO($products);
+        $productsDTO = new CollectionDTO();
+
+        foreach ($products as $product) {
+            $productsDTO->pushItem(new ProductDTO($product));
+        }
+
+        $paginateDTO->setData($productsDTO);
+
+        return response()->json($paginateDTO);
     }
 }
